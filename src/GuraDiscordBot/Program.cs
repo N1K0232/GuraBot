0a103ad652da372fa76025de1b67d869421cc042
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-05-07 10:24:14 +0200

    initial setup

--- src/GuraDiscordBot/Program.cs
+++ src/GuraDiscordBot/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GuraDiscordBot;
+
+public class Program
+{
+    public static void Main()
+    {
+    }
+}
